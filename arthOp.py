82d1fecb5174d2d36dc8b547de13af8ed8a6ef70
mudigonda <himansh02@hotmail.com>
2020-10-15 22:22:34 +0530

    arithmetic operations using functions

---
+++ arthOp.py
@@ -0,0 +1,29 @@
+
+def main():
+	add(13,2)
+	sub(13,6)
+	mul(12,5)
+	mod(12,2)
+	div(6,2)
+
+def add(a,b):
+	c = a+b
+	print(c)
+
+def sub(a,b):
+	c = a-b
+	print(c)
+
+def mul(a,b):
+	c = a*b
+	print(c)
+
+def mod(a,b):
+	c = a%b
+	print(c)
+
+def div(a,b):
+	c = a/b
+	print(c)
+
+main()
